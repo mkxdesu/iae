399eec57f793b86f8b983713feec50966e6427d8
mkxdesu <mkxdesu@gmail.com>
2026-04-19 11:17:54 +0700

    Fixed Student Factory

--- Tugas 2/database/factories/StudentFactory.php
+++ Tugas 2/database/factories/StudentFactory.php
@@ -1,67 +1,27 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace Database\Factories;
 
-use Illuminate\Http\Request;
-use App\Models\Student;
-use App\Http\Resources\studentResource;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-class StudentController extends Controller
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
 {
-    public function index()
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
     {
-        $students = Student::all();
-        return new studentResource($students, 'Success', 'List of students');
-    }
-
-    public function store(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'nim' => 'required',
-            'name' => 'required',
-            'email' => 'required',
-            'address' => 'required',
-            'phone' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return new studentResource(null, 'Failed', $validator->errors());
-        }
-
-        $student = Student::create($request->all());
-        return new studentResource($student, 'Success', 'Student created successfully');
-    }
-
-    public function show($id)
-    {
-        $student = Student::find($id);
-        if ($student) {
-            return new studentResource($student, 'Success', 'Student found');
-        } else {
-            return new studentResource(null, 'Failed', 'Student not found');
-        }
-    }
-
-    public function update(Request $request, $id)
-    {
-        $student = Student::find($id);
-        if ($student) {
-            $student->update($request->all());
-            return new studentResource($student, 'Success', 'Student updated successfully');
-        } else {
-            return new studentResource(null, 'Failed', 'Student not found');
-        }
-    }
-
-    public function destroy($id)
-    {
-        $student = Student::find($id);
-        if ($student) {
-            $student->delete();
-            return new studentResource($student, 'Success', 'Student deleted successfully');
-        } else {
-            return new studentResource(null, 'Failed', 'Student not found');
-        }
+        return [
+            'nim' => $this->faker->unique()->randomNumber(8),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'address' => $this->faker->address(),
+            'phone' => $this->faker->phoneNumber(),
+        ];
     }
 }
